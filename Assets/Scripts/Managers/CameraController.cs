ba77636ce867432d80f95a49b1550c4a500951b1
adam russell <badamrussell@users.noreply.github.com>
2021-02-25 05:53:43 -0500

    fix(camera): each side of screen has min/max so when mouse leaves screen prevents additional panning

--- Assets/Scripts/Managers/CameraController.cs
+++ Assets/Scripts/Managers/CameraController.cs
@@ -21,15 +21,23 @@ namespace GameDevHQITP.Managers
 
         [SerializeField] private float _mouseEdgePanSpeed = 0.1f;
         [SerializeField] private float _screenEdgePanningOffset = 100f;
-        private float[] _screenLimitsHorz = new float[2];
-        private float[] _screenLimitsVert = new float[2];
+        [SerializeField] private float _screenEdgePanningBottomOffset = 50f;
+        [SerializeField] private float _screenEdgeGutter = 1f;
+
+        private float[] _screenLimitsHorz = new float[4];
+        private float[] _screenLimitsVert = new float[4];
 
         private void Start()
         {
-            _screenLimitsHorz[0] = 0 + _screenEdgePanningOffset;
-            _screenLimitsHorz[1] = Screen.width - _screenEdgePanningOffset;
-            _screenLimitsVert[0] = 0 + _screenEdgePanningOffset;
-            _screenLimitsVert[1] = Screen.height - _screenEdgePanningOffset;
+            _screenLimitsHorz[0] = _screenEdgeGutter;
+            _screenLimitsHorz[1] = 0 + _screenEdgePanningOffset;
+            _screenLimitsHorz[2] = Screen.width - _screenEdgePanningOffset;
+            _screenLimitsHorz[3] = Screen.width - _screenEdgeGutter;
+
+            _screenLimitsVert[0] = 0 + _screenEdgeGutter;
+            _screenLimitsVert[1] = 0 + _screenEdgePanningBottomOffset;
+            _screenLimitsVert[2] = Screen.height - _screenEdgePanningOffset;
+            _screenLimitsVert[3] = Screen.height - _screenEdgeGutter;
         }
 
         void Update()
@@ -57,20 +65,20 @@ namespace GameDevHQITP.Managers
             float mouseX = Input.mousePosition.x;
             float mouseY = Input.mousePosition.y;
 
-            if (mouseX < _screenLimitsHorz[0])
+            if (mouseX > _screenLimitsHorz[0] && mouseX < _screenLimitsHorz[1])
             {
                 pos.x -= _mouseEdgePanSpeed;
             }
-            else if (mouseX > _screenLimitsHorz[1])
+            else if (mouseX > _screenLimitsHorz[2] && mouseX < _screenLimitsHorz[3])
             {
                 pos.x += _mouseEdgePanSpeed;
             }
 
-            if (mouseY < _screenLimitsVert[0])
+            if (mouseY > _screenLimitsVert[0] && mouseY < _screenLimitsVert[1])
             {
                 pos.z -= _mouseEdgePanSpeed;
             }
-            else if (mouseY > _screenLimitsVert[1])
+            else if (mouseY > _screenLimitsVert[2] && mouseY < _screenLimitsVert[3])
             {
                 pos.z += _mouseEdgePanSpeed;
             }
